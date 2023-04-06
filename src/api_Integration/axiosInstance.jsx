b2cdf40294ed63da0bcfeb7f62121d39157fe316
Hariprasad K H <hariprasad72172@gmail.com>
2023-04-06 20:05:23 +0530

    bugs solved

--- src/api_Integration/axiosInstance.jsx
+++ src/api_Integration/axiosInstance.jsx
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 export const axiosInstanceAdmin = axios.create({
-    baseURL : 'https://cineawe-server.cineawe.online/api/admin'
+    baseURL : 'https://www.cineawe-server.cineawe.online/api/admin'
 })
 
 export const axiosInstanceUser = axios.create({
-    baseURL : 'https://cineawe-server.cineawe.online/api/users'
+    baseURL : 'https://www.cineawe-server.cineawe.online/api/users'
 })
 
 export const axiosInstanceOwner = axios.create({
-    baseURL : 'https://cineawe-server.cineawe.online/api/owner'
+    baseURL : 'https://www.cineawe-server.cineawe.online/api/owner'
 })
 
 export const axiosInstanceMovie = axios.create({
